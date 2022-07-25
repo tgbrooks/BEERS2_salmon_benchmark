bb4f275113ecbf6276077d8c491dc12e5ba85448
tgbrooks <tgbrooks@gmail.com>
2022-07-25 16:52:58 -0400

    Display normalized GC content

--- scripts/plot_gc_content.py
+++ scripts/plot_gc_content.py
@@ -6,6 +6,10 @@ outdir = pathlib.Path(snakemake.output[0])
 outdir.mkdir(exist_ok=True)
 
 summary = pandas.read_csv(snakemake.input.gc_summary, sep="\t")
+total_count = summary.groupby(['run', 'sample'])['count'].sum()
+total_count.name = 'total_count'
+summary = pandas.merge(summary, total_count.reset_index(), on=['run', 'sample'])
+summary['normalized_count'] = summary['count'] / summary['total_count']
 
 
 fig = sns.relplot(
@@ -16,4 +20,14 @@ fig = sns.relplot(
         data = summary,
         kind = "line",
 )
-fig.savefig(outdir / "summary.png")
+fig.savefig(outdir / "summary.png", dpi=300)
+
+fig = sns.relplot(
+        y = "normalized_count",
+        x = "GC_content",
+        hue = "run",
+        col = "sample",
+        data = summary,
+        kind = "line",
+)
+fig.savefig(outdir / "summary.normalized.png", dpi=300)
