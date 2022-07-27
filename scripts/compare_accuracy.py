9923f1980c50b3d99378dfc4b387589737c1973d
tgbrooks <tgbrooks@gmail.com>
2022-07-27 12:03:29 -0400

    Add MADR plot, move to scatter plots not bar plots

--- scripts/compare_accuracy.py
+++ scripts/compare_accuracy.py
@@ -39,31 +39,38 @@ true_counts.rename(columns={"count": "true_count"}, inplace=True)
 data = pandas.merge(salmon, true_tpm, on=['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'TranscriptID', 'GeneID'])
 data = pandas.merge(data, true_counts, on=['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'TranscriptID', 'GeneID'])
 data = pandas.merge(data, beers_in, on=['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'TranscriptID', 'GeneID'])
-print(data.head())
 
 # Compute CPM values
 data['true_cpm'] = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct'])['true_count'].apply( lambda x: x / x.sum() * 1_000_000)
 data['salmon_cpm'] = data.groupby(['sample','run',  'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct'])['NumReads'].apply( lambda x: x / x.sum() * 1_000_000)
 
+#  Determine the number of isoforms/transcripts in a each gene
+num_isoforms = data.groupby(['GeneID']).TranscriptID.nunique()
+data['num_isoforms'] = data.GeneID.map(num_isoforms)
+
+print(data.head())
+print(data.shape)
+
 # Sum all transcripts in a gene to get gene-level data
 gene_data = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct', 'GeneID']).apply(lambda x: x[['TPM', 'salmon_cpm', 'NumReads', 'true_count', 'true_tpm', 'true_cpm']].sum(axis=0)).reset_index()
 
-correction_type_order = ['baseline', 'GC', 'Pos', 'GC Pos', 'Seq', 'GC Seq', 'Pos Seq', 'GC Pos Seq']
+correction_type_order = ['baseline', 'Seq', 'Pos', 'Pos Seq', 'GC', 'GC Seq', 'GC Pos', 'GC Pos Seq']
 def correction_type(row):
     ''' Summarize all Salmon correction options in one string '''
     corrections = [correction for correction in ['GC', 'Pos', 'Seq'] if row[f'{correction}_correct']]
     if corrections:
         return ' '.join(corrections)
     return 'baseline'
+data['correction type'] = data.apply(correction_type, axis=1)
 
 ## Summary stats
 ## Spearman correlations
-count_corr = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct']).apply(lambda x: scipy.stats.spearmanr(x.NumReads, x.true_count, nan_policy='omit')[0])
-tpm_corr = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct']).apply(lambda x: scipy.stats.spearmanr(x.TPM, x.true_tpm, nan_policy='omit')[0])
+count_corr = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).apply(lambda x: scipy.stats.spearmanr(x.NumReads, x.true_count, nan_policy='omit')[0])
+tpm_corr = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).apply(lambda x: scipy.stats.spearmanr(x.TPM, x.true_tpm, nan_policy='omit')[0])
 # Absolute median deviation - log2 scale
-count_abs_med_dev = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct']).apply(lambda x: numpy.abs(numpy.log2((x['NumReads'] + 1) / (x['true_count']+1))).median())
-tpm_abs_med_dev = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct']).apply(lambda x: numpy.abs(numpy.log2((x['TPM'] + 1) / (x['true_tpm']+1))).median())
-cpm_abs_med_dev = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct']).apply(lambda x: numpy.abs(numpy.log2((x['salmon_cpm'] + 1) / (x['true_cpm']+1))).median())
+count_abs_med_dev = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).apply(lambda x: numpy.abs(numpy.log2((x['NumReads'] + 1) / (x['true_count']+1))).median())
+tpm_abs_med_dev = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).apply(lambda x: numpy.abs(numpy.log2((x['TPM'] + 1) / (x['true_tpm']+1))).median())
+cpm_abs_med_dev = data.groupby(['sample', 'run', 'GC_bias', 'pos_3prime_bias', 'primer_bias', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).apply(lambda x: numpy.abs(numpy.log2((x['salmon_cpm'] + 1) / (x['true_cpm']+1))).median())
 
 stats = pandas.DataFrame({
     "count_corr": count_corr,
@@ -78,36 +85,35 @@ stats['bias_correct'] = stats['GC_correct'].map({True: "GC ", False: ""}) + stat
 # Plot correlation summary stats
 fig = sns.catplot(
     data=stats.query("sample == 1"),
-    x="bias_correct",
+    x="run",
     y="count_corr",
-    col="run",
-    col_wrap=3,
-    col_order=run_order,
-    kind="bar",
+    hue = "correction type",
+    hue_order = correction_type_order,
+    order = run_order,
 )
+[ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
 fig.savefig(out_dir / "count_corr.png", dpi=300)
 
 # Plot TPM correlation summary stats
 fig = sns.catplot(
     data=stats.query("sample == 1"),
-    x="bias_correct",
+    x="run",
     y="tpm_corr",
-    col="run",
-    col_wrap=3,
-    col_order=run_order,
-    kind="bar",
+    hue = "correction type",
+    hue_order = correction_type_order,
+    order = run_order,
 )
+[ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
 fig.savefig(out_dir / "tpm_corr.png", dpi=300)
 
 # Plot abs med dev summary stats
 fig = sns.catplot(
     data=stats.query("sample == 1"),
-    x="bias_correct",
+    x="run",
     y="count_abs_med_dev",
-    col="run",
-    col_wrap=3,
-    col_order=run_order,
-    kind="bar",
+    hue = "correction type",
+    hue_order = correction_type_order,
+    order = run_order,
 )
 [ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
 fig.savefig(out_dir / "count_abs_med_dev.png", dpi=300)
@@ -115,12 +121,11 @@ fig.savefig(out_dir / "count_abs_med_dev.png", dpi=300)
 # Plot TPM abs med dev summary stats
 fig = sns.catplot(
     data=stats.query("sample == 1"),
-    x="bias_correct",
+    x="run",
     y="tpm_abs_med_dev",
-    col="run",
-    col_wrap=3,
-    col_order=run_order,
-    kind="bar",
+    hue = "correction type",
+    hue_order = correction_type_order,
+    order = run_order,
 )
 [ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
 fig.savefig(out_dir / "tpm_abs_med_dev.png", dpi=300)
@@ -128,12 +133,11 @@ fig.savefig(out_dir / "tpm_abs_med_dev.png", dpi=300)
 # Plot CPM abs med dev summary stats
 fig = sns.catplot(
     data=stats.query("sample == 1"),
-    x="bias_correct",
+    x="run",
     y="cpm_abs_med_dev",
-    col="run",
-    col_wrap=3,
-    col_order = run_order,
-    kind="bar",
+    hue = "correction type",
+    hue_order = correction_type_order,
+    order = run_order,
 )
 [ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
 fig.savefig(out_dir / "cpm_abs_med_dev.png", dpi=300)
@@ -209,7 +213,8 @@ fig = sns.displot(
     col = "run",
     col_wrap = 3,
     kind = "kde",
-    clip = (-2.5, 2.5),
+    facet_kws = {"sharey": False},
+    clip = (-2.0, 2.0),
     col_order = run_order,
     hue_order = correction_type_order,
     common_norm = False,
@@ -236,14 +241,15 @@ fig = sns.displot(
     col = "run",
     col_wrap = 3,
     kind = "kde",
-    clip = (-2.5, 2.5),
+    facet_kws = {"sharey": False},
+    clip = (-2.0, 2.0),
     col_order = run_order,
     hue_order = correction_type_order,
     common_norm = False,
 )
 fig.refline(x=0)
 fig.set_axis_labels("log2( |TPM err| / |TPM baseline error|)", "Gene Density")
-print("TPM Frac in range:", (d['log_rel_abs_error'].abs() < 2.5).mean())
+print("TPM Frac in range:", (d['log_rel_abs_error'].abs() < 2.0).mean())
 fig.savefig(out_dir / "abs_error.compared_to_baseline.by_gene.png", dpi=300)
 
 # CPM Transcript-level:
@@ -264,7 +270,8 @@ fig = sns.displot(
     col = "run",
     col_wrap = 3,
     kind = "kde",
-    clip = (-2.5, 2.5),
+    clip = (-2.0, 2.0),
+    facet_kws = {"sharey": False},
     col_order = run_order,
     hue_order = correction_type_order,
     common_norm = False,
@@ -291,14 +298,15 @@ fig = sns.displot(
     col = "run",
     col_wrap = 3,
     kind = "kde",
-    clip = (-2.5, 2.5),
+    clip = (-2.0, 2.0),
+    facet_kws = {"sharey": False},
     col_order = run_order,
     hue_order = correction_type_order,
     common_norm = False,
 )
 fig.refline(x=0)
 fig.set_axis_labels("log2( |CPM err| / |CPM baseline error|)", "Gene Density")
-print("CPM Frac in range:", (d['log_rel_abs_error'].abs() < 2.5).mean())
+print("CPM Frac in range:", (d['log_rel_abs_error'].abs() < 2.0).mean())
 fig.savefig(out_dir / "abs_error.compared_to_baseline.CPM.by_gene.png", dpi=300)
 
 
@@ -314,8 +322,8 @@ fig.savefig(out_dir / "abs_error.compared_to_baseline.CPM.by_gene.png", dpi=300)
 
 # For GC bias:
 d = data.copy()
-GC_baseline = d.query("pos_3prime_bias == 'none' and GC_correct == False and Pos_correct == False").set_index(['GC_bias', 'sample', 'TranscriptID'])
-GC_corrected = d.query("pos_3prime_bias == 'none' and GC_correct == True and Pos_correct == False").set_index(['GC_bias', 'sample', 'TranscriptID'])
+GC_baseline = d.query("pos_3prime_bias == 'none' and primer_bias == 'none' and GC_correct == False and Pos_correct == False and Seq_correct == False").set_index(['GC_bias', 'sample', 'TranscriptID'])
+GC_corrected = d.query("pos_3prime_bias == 'none'and primer_bias == 'none' and GC_correct == True and Pos_correct == False and Seq_correct == False").set_index(['GC_bias', 'sample', 'TranscriptID'])
 GC_correction = pandas.DataFrame({
     "GC_bias_factor":  GC_corrected.EffectiveLength / GC_baseline.EffectiveLength,
     "rel_abs_error": ((GC_corrected.TPM - GC_corrected.true_tpm) / (GC_baseline.TPM - GC_baseline.true_tpm)).abs(),
@@ -349,8 +357,8 @@ fig = sns.displot(
 fig.savefig(out_dir / "GC_bias_factor.dist.png", dpi=300)
 
 # Same for Positional bias
-pos_baseline = d.query("GC_bias == 'none' and GC_correct == False and Pos_correct == False").set_index(['pos_3prime_bias', 'sample', 'TranscriptID'])
-pos_corrected = d.query("GC_bias == 'none' and GC_correct == False and Pos_correct == True").set_index(['pos_3prime_bias', 'sample', 'TranscriptID'])
+pos_baseline = d.query("GC_bias == 'none' and primer_bias == 'none' and GC_correct == False and Pos_correct == False and Seq_correct == False").set_index(['pos_3prime_bias', 'sample', 'TranscriptID'])
+pos_corrected = d.query("GC_bias == 'none' and primer_bias == 'none' and GC_correct == False and Pos_correct == True and Seq_correct == False").set_index(['pos_3prime_bias', 'sample', 'TranscriptID'])
 pos_correction = pandas.DataFrame({
     "pos_bias_factor":  pos_corrected.EffectiveLength / pos_baseline.EffectiveLength,
     "rel_abs_error": ((pos_corrected.TPM - pos_corrected.true_tpm) / (pos_baseline.TPM - pos_baseline.true_tpm)).abs(),
@@ -384,29 +392,99 @@ fig = sns.displot(
 fig.savefig(out_dir / "pos_bias_factor.dist.png", dpi=300)
 
 
+# Same for sequence bias
+seq_baseline = d.query("GC_bias == 'none' and pos_3prime_bias == 'none' and GC_correct == False and Pos_correct == False and Seq_correct == False").set_index(['primer_bias', 'sample', 'TranscriptID'])
+seq_corrected = d.query("GC_bias == 'none' and pos_3prime_bias == 'none' and GC_correct == False and Pos_correct == False and Seq_correct == True").set_index(['primer_bias', 'sample', 'TranscriptID'])
+seq_correction = pandas.DataFrame({
+    "seq_bias_factor":  seq_corrected.EffectiveLength / seq_baseline.EffectiveLength,
+    "rel_abs_error": ((seq_corrected.TPM - seq_corrected.true_tpm) / (seq_baseline.TPM - seq_baseline.true_tpm)).abs(),
+    "baseline_num_reads": seq_baseline.NumReads,
+    "log10_baseline_num_reads": numpy.log10(seq_baseline.NumReads + 1),
+}).reset_index()
+fig = sns.relplot(
+        x = "seq_bias_factor",
+        y = "rel_abs_error",
+        size = "log10_baseline_num_reads",
+        data = seq_correction.query(f"baseline_num_reads > {MIN_NUM_READS}"),
+        col = "sample",
+        row = "primer_bias",
+        row_order=bias_order,
+        kind = "scatter",
+        #kind = "kde",
+        #common_norm = False,
+)
+fig.set(ylim=(0.0, 2.0))
+fig.refline(y=1)
+fig.savefig(out_dir / "rel_abs_err.by_seq_bias_factor.png", dpi=300)
+fig = sns.displot(
+        x = "seq_bias_factor",
+        data = seq_correction.query(f"baseline_num_reads > {MIN_NUM_READS}"),
+        col = "sample",
+        hue = "primer_bias",
+        hue_order=bias_order,
+        kind = "kde",
+        common_norm = False,
+)
+fig.savefig(out_dir / "seq_bias_factor.dist.png", dpi=300)
+
+## Median absolute relative difference (MARD)
+# Transcript-level MADR in both counts and TPMs compared to truth
+data['ARD_count'] = (data['NumReads'] - data['true_count']).abs() / (data['NumReads'] + data['true_count'] + 1e-10) # Small epislon means that it goes to zero properly
+data['ARD_tpm'] = (data['TPM'] - data['true_tpm']).abs() / (data['TPM'] + data['true_tpm'] + 1e-10)
+MARD_count = data.groupby(['run', 'sample', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).ARD_count.median()
+MARD_count.name = "MARD count"
+MARD_tpm = data.groupby(['run', 'sample', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type']).ARD_tpm.median()
+MARD_tpm.name = "MARD TPM"
+fig = sns.catplot(
+        data = MARD_count.reset_index().query("sample == 1"),
+        x = "run",
+        y = "MARD count",
+        hue = "correction type",
+        hue_order = correction_type_order,
+        order = run_order,
+)
+[ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
+fig.savefig(out_dir / "MARD.counts.png", dpi=300)
+fig = sns.catplot(
+        data = MARD_tpm.reset_index().query("sample == 1"),
+        x = "run",
+        y = "MARD TPM",
+        hue = "correction type",
+        hue_order = correction_type_order,
+        order = run_order,
+)
+[ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
+fig.savefig(out_dir / "MARD.tpm.png", dpi=300)
 
 
-## Generate scatterplot figures
-# NOTE: these aren't so useful for the full genome
-#import pylab
-#import math
-#for measure in ['tpm', 'count']:
-#    truth_var = {"count": "true_count", "tpm": "true_tpm"}[measure]
-#    quant_var = {"count": "NumReads", "tpm": "TPM"}[measure]
-#    for sample in data['sample'].unique():
-#        by_study = data[data['sample'] == sample].set_index(["GC_bias", "pos_3prime_bias", "GC_correct", "Pos_correct"])
-#        conditions = by_study.index.unique()
-#        N_COLS = 4
-#        N_ROWS = math.ceil(len(conditions) / N_COLS)
-#        fig, axes = pylab.subplots(nrows=N_ROWS, ncols=N_COLS, figsize=(20,20), sharex=True, sharey=True)
-#        for condition, ax in zip(conditions, axes.flatten()):
-#            cond_data = by_study.loc[condition]
-#            ax.loglog(cond_data[truth_var], cond_data[quant_var], color="k", linestyle='', marker='o')
-#            #ax.set_aspect(1.0)
-#            ax.set_title(condition)
-#        for ax in axes[-1,:]:
-#            ax.set_xlabel("True")
-#        for ax in axes[:,0]:
-#            ax.set_ylabel("Est.")
-#        #fig.tight_layout()
-#        fig.savefig(f"{snakemake.output.dir}/{measure}.scatter.sample{sample}.png", dpi=150)
+
+## MARD by number of isoforms
+data['num isoforms'] = pandas.cut(data['num_isoforms'], [1,2,3,5,10,data.num_isoforms.max()])
+MARD_count = data.groupby(['run', 'sample', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type', 'num isoforms']).ARD_count.median()
+MARD_count.name = "MARD count"
+MARD_tpm = data.groupby(['run', 'sample', 'GC_correct', 'Pos_correct', 'Seq_correct', 'correction type', 'num isoforms']).ARD_tpm.median()
+MARD_tpm.name = "MARD TPM"
+fig = sns.catplot(
+        data = MARD_count.reset_index().query("sample == 1"),
+        x = "run",
+        y = "MARD count",
+        col = "num isoforms",
+        col_wrap = 3,
+        hue = "correction type",
+        hue_order = correction_type_order,
+        order = run_order,
+)
+[ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
+fig.savefig(out_dir / "MARD.counts.by_num_isoforms.png", dpi=300)
+fig = sns.catplot(
+        data = MARD_tpm.reset_index().query("sample == 1"),
+        x = "run",
+        y = "MARD TPM",
+        col = "num isoforms",
+        col_wrap = 3,
+        hue = "correction type",
+        hue_order = correction_type_order,
+        order = run_order,
+)
+[ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right') for ax in fig.axes.flatten()]
+fig.savefig(out_dir / "MARD.tpm.by_num_isoforms.png", dpi=300)
